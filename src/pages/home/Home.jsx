a5dec5d8888d6b4381c47001b2c6e82226e7c9b7
yunus <yunuspolat2528@gmail.com>
2023-09-06 16:29:04 +0200

    Form was added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 import CardCountry from './CardCountry';
+import Form from "react-bootstrap/Form";
 
 const Home = () => {
     const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState("")
   const url = "https://restcountries.com/v3.1/all";
 
   const getData = async () => {
@@ -29,14 +31,27 @@ const Home = () => {
 console.log(countries);
   return (
     <Container className="my-4">
-      <Row >
-        {countries.map((country, index) => {
-          return (
-            <Col key={index} xl={3} lg={4} md={6} sm={12} className="g-4">
-              <CardCountry country={country} />
-            </Col>
-          );
-        })}
+      <Form className="d-flex justify-content-center ">
+        <Form.Group className="mb-3 w-50" controlId="formBasicEmail">
+          <Form.Control
+            type="search"
+            placeholder="Search a country..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <Row>
+        {countries
+          .filter((country) =>
+            country.name.official.toLowerCase().includes(search.trim().toLowerCase())
+          )
+          .map((country, index) => {
+            return (
+              <Col key={index} xl={3} lg={4} md={6} sm={12} className="g-4">
+                <CardCountry country={country} />
+              </Col>
+            );
+          })}
       </Row>
     </Container>
   );
